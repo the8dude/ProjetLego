555de905a0e35608209db6f373edcce489265c2c
DuDe <dumontderek@gmail.com>
2016-03-07 10:54:14 +0100

    Ajout photos

--- Projet D/Form1.Designer.cs
+++ Projet D/Form1.Designer.cs
@@ -80,7 +80,7 @@
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Projet D";
+            this.Text = "Projet Lego";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
